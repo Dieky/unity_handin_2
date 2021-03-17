b88a25a9cfd68b221c457fb63b0cc7caef6ebeef
Patrick <test@test.com>
2021-03-17 19:59:34 +0100

    added endzone to beach

---
+++ Assets/Endzone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endzone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float chances = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ENDZONE REACHED");
+        chances -= 1f;
+        Debug.Log(chances);
+        Destroy(other.gameObject);
+    }
+    
+    // Update is called once per frame
+   
+}
